e42e889db92c20db8867bd1244c8b3fc0f1f88c1
Jóel <Bassitruma@gmail.com>
2024-12-12 02:55:09 +0000

    created the form (empty)

---
+++ web/modules/custom/music_search/tests/Form/CreationForm.php
@@ -0,0 +1,11 @@
+<?php
+namespace Drupal\music_search\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class CreationForm extends FormBase {
+  public function getFormId() {}
+  public function buildForm(array $form, FormStateInterface $form_state) {}
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
+}
