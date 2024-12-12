2ca6480a4fbf508630921c8eea4e6ea0b681dd4a
Joel <bassitruma@gmail.com>
2024-12-12 14:40:15 +0000

    all forms exist and are filled, do not have any functionality

--- web/modules/custom/music_search/src/Form/AlbumForm.php
+++ web/modules/custom/music_search/src/Form/AlbumForm.php
@@ -5,17 +5,20 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormInterface;
 use Drupal\Core\Form\FormStateInterface;
 
-class CreationForm extends FormBase {
+class AlbumForm extends FormBase {
   public function getFormId() {
-    return 'music_search_song_form';
+    return 'music_search_album_form';
   }
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['type'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Type'),
-      '#options' => ['album' =>$this->t('Album'), 'song'=>$this->t('Song'), 'artist'=>$this->t('Artist')],
+    $form['Album'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Album Name'),
       '#required' => TRUE,
-      '#default_value' => t('Album'),
+    ];
+    $form['Artist'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Artist Name'),
+      '#required' => TRUE,
     ];
     $form['submit'] = [
       '#type' => 'submit',
--- web/modules/custom/music_search/src/Form/SongForm.php
+++ web/modules/custom/music_search/src/Form/SongForm.php
@@ -5,17 +5,20 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormInterface;
 use Drupal\Core\Form\FormStateInterface;
 
-class CreationForm extends FormBase {
+class SongForm extends FormBase {
   public function getFormId() {
     return 'music_search_song_form';
   }
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['type'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Type'),
-      '#options' => ['album' =>$this->t('Album'), 'song'=>$this->t('Song'), 'artist'=>$this->t('Artist')],
+    $form['Song'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Song Name'),
       '#required' => TRUE,
-      '#default_value' => t('Album'),
+    ];
+    $form['Artist'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Artist Name'),
+      '#required' => TRUE,
     ];
     $form['submit'] = [
       '#type' => 'submit',
